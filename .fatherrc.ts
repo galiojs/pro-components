2e8ba3eb0956087308ed5e875813ea3898a74c34
Jerry W <wangguoguo.lan@gmail.com>
2022-04-26 09:46:05 +0800

    chore: remove "module"

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -31,7 +31,11 @@ if (type === 'lib') {
     runtimeHelpers: true,
     pkgs: [...headPkgs, ...tailPkgs],
     extraBabelPlugins: [
-      ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'lib', style: false }, 'antd'],
+      [
+        'babel-plugin-import',
+        { libraryName: 'antd', libraryDirectory: 'lib', style: false },
+        'antd',
+      ],
     ],
   };
 }
@@ -46,7 +50,11 @@ if (type === 'es') {
     pkgs: [...headPkgs, ...tailPkgs],
     extraBabelPlugins: [
       [require('./scripts/replaceLib')],
-      ['babel-plugin-import', { libraryName: 'antd', libraryDirectory: 'es', style: false }, 'antd'],
+      [
+        'babel-plugin-import',
+        { libraryName: 'antd', libraryDirectory: 'es', style: false },
+        'antd',
+      ],
     ],
   };
 }
